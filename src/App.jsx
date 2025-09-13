888168ec0bf0330482b0a89f55a5715a367ade7c
anuchaj <anuchaj50@gmail.com>
2025-09-13 23:01:12 +0100

    all in one file

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,33 @@ export default function App() {
   function handleSubmit(e) {
     e.preventDefault()
 
-    setTodos([
-      ...todos,
-      { id: crypto.randomUUID(), title: newItem, completed: false },
-    ])
+    setTodos (currentTodos => {
+      return [
+        ...currentTodos, 
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+
+    setNewItem("")
   }
 
-  console.log(todos)
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id ) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
     <>
@@ -32,20 +52,23 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.length === 0 && "No Todos"}
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+            <label>
+              <input 
+                type="checkbox" 
+                checked={todo.completed}
+                onChange={e => toggleTodo(todo.id, e.target.checked)} 
+              />
+              {todo.title}
+            </label>
+            <button onClick={() => deleteTodo(todo.id)} 
+            className="btn btn-danger">Delete</button>
+          </li>
+          )
+        })}
       </ul>
     </>
   )
